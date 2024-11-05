51d7710ee3dc4960751c038167d7e862b11910d9
Jude Adarkwa <jadarkwa@umich.edu>
2024-11-05 12:31:23 -0500

    scroll list

--- lib/helps.dart
+++ lib/helps.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
-import "package:latlong2/latlong.dart" as lat_lng;
+import 'package:latlong2/latlong.dart' as lat_lng;
 
-class HelpsPage extends StatelessWidget {
+class HelpsPage extends StatefulWidget {
   const HelpsPage({super.key});
+
+  @override
+  State<HelpsPage> createState() => _HelpsPageState();
+}
+
+class _HelpsPageState extends State<HelpsPage> {
+  double _sheetPosition = 0.5;
+  final double _dragSensitivity = 600;
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +44,10 @@ class HelpsPage extends StatelessWidget {
       ),
       body: Stack(
         children: [
+          // Map Layer
           FlutterMap(
             options: const MapOptions(
-              initialCenter:
-                  lat_lng.LatLng(36.1716, -115.1391), // Las Vegas, Nevada
+              initialCenter: lat_lng.LatLng(36.1716, -115.1391), // Las Vegas, Nevada
               minZoom: 1,
             ),
             children: [
@@ -49,7 +57,106 @@ class HelpsPage extends StatelessWidget {
               ),
             ],
           ),
+          DraggableScrollableSheet(
+            initialChildSize: _sheetPosition,
+            minChildSize: 0.2,
+            maxChildSize: 0.8,
+            builder: (context, scrollController) {
+           return ColoredBox(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Grabber(
+                      onVerticalDragUpdate: (details) {
+                        setState(() {
+                          _sheetPosition -= details.delta.dy / _dragSensitivity;
+                          _sheetPosition = _sheetPosition.clamp(0.2, 0.8);
+                        });
+                      },
+                    ),
+                  Expanded(
+                    child: ListView.builder(
+                      controller: scrollController,
+                      padding: EdgeInsets.zero,
+                       itemCount: 20, 
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0), 
+                          child: Container(
+                            padding: const EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300], 
+                              borderRadius: BorderRadius.circular(12.0), 
+                            ),
+                            child: Row(
+                               children: [
+                                Icon(Icons.location_on, color: Colors.grey[700]), 
+                                const SizedBox(width: 16.0),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                       'Location $index',
+                                      style: const TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black87,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 40.0),
+                                     Text(
+                                      'Description of location $index',
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.grey[800],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                   ),
+                  ],
+                ),
+              );
+            },
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class Grabber extends StatelessWidget {
+  const Grabber({
+    super.key,
+    required this.onVerticalDragUpdate,
+  });
+
+  final ValueChanged<DragUpdateDetails> onVerticalDragUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragUpdate: onVerticalDragUpdate,
+      child: Container(
+         width: double.infinity,
+        color: Colors.grey[300],
+        child: Center(
+           child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            width: 32.0,
+           height: 4.0,
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+       ),
       ),
     );
   }

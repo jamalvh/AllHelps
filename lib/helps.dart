46b76aa0eff9346c51890a4dc8c8c0ebcfb8421f
Aman-Amith-Shastry <amanshastrychess@gmail.com>
2024-11-05 21:31:04 -0500

    Added resource list

--- lib/helps.dart
+++ lib/helps.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as lat_lng;
@@ -14,6 +13,38 @@ class _HelpsPageState extends State<HelpsPage> {
   double _sheetPosition = 0.5;
   final double _dragSensitivity = 600;
 
+  List<String> chosenFilters = [];
+  final Map<String, List<String>> filters = {
+    "Food": ["Food and Meals- Hot Meals", "Food bank"],
+    "Support": [
+      "Abuse/Stalking- locations",
+      "Child care",
+      "Donation Centers",
+      "Financial Guidance & Management",
+      "Foster care service"
+    ],
+    "Medical": [],
+    "Shelter": [
+      "Cooling station",
+      "Housing",
+      "Housing Assistance/Coordinated Entry",
+      "Laundry",
+      "Shelter",
+      "Showers"
+    ],
+    "Resources": [
+      "Bus ticket home",
+      "Clothing",
+      "Community Resources",
+      "Coordinated Energy Assessment Sites",
+      "Energy SHARE",
+      "Families with Children",
+      "Disability Housing Program",
+      "Disability Services"
+    ],
+    "Other": []
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,22 +52,28 @@ class _HelpsPageState extends State<HelpsPage> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         toolbarHeight: MediaQuery.of(context).size.height * 0.15,
-        title: const Column(
-          children: [
-            SearchBar(
-              leading: Icon(Icons.search),
-              hintText: 'I want to find ...',
-              backgroundColor: WidgetStatePropertyAll(Colors.white),
-              side: WidgetStatePropertyAll(BorderSide(color: Colors.black12)),
-              shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)))),
-              elevation: WidgetStatePropertyAll(0),
-            ),
-          ],
-        ),
+        title: Column(children: [
+          const SearchBar(
+            leading: Icon(Icons.search),
+            hintText: 'I want to find ...',
+            backgroundColor: WidgetStatePropertyAll(Colors.white),
+            side: WidgetStatePropertyAll(BorderSide(color: Colors.black12)),
+            shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15)))),
+            elevation: WidgetStatePropertyAll(0),
+          ),
+          ListView(scrollDirection: Axis.horizontal, children: [])
+        ]),
         actions: [
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.filter_alt_off_sharp))
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.filter_alt_off_sharp),
+              ),
+              const Text('Filter')
+            ],
+          )
         ],
         backgroundColor: Colors.white,
         shape: const RoundedRectangleBorder(
@@ -47,7 +84,8 @@ class _HelpsPageState extends State<HelpsPage> {
           // Map Layer
           FlutterMap(
             options: const MapOptions(
-              initialCenter: lat_lng.LatLng(36.1716, -115.1391), // Las Vegas, Nevada
+              initialCenter:
+                  lat_lng.LatLng(36.1716, -115.1391), // Las Vegas, Nevada
               minZoom: 1,
             ),
             children: [
@@ -62,7 +100,7 @@ class _HelpsPageState extends State<HelpsPage> {
             minChildSize: 0.2,
             maxChildSize: 0.8,
             builder: (context, scrollController) {
-           return ColoredBox(
+              return ColoredBox(
                 color: Colors.white,
                 child: Column(
                   children: [
@@ -74,52 +112,55 @@ class _HelpsPageState extends State<HelpsPage> {
                         });
                       },
                     ),
-                  Expanded(
-                    child: ListView.builder(
-                      controller: scrollController,
-                      padding: EdgeInsets.zero,
-                       itemCount: 20, 
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0), 
-                          child: Container(
-                            padding: const EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[300], 
-                              borderRadius: BorderRadius.circular(12.0), 
-                            ),
-                            child: Row(
-                               children: [
-                                Icon(Icons.location_on, color: Colors.grey[700]), 
-                                const SizedBox(width: 16.0),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                       'Location $index',
-                                      style: const TextStyle(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black87,
+                    Expanded(
+                      child: ListView.builder(
+                        controller: scrollController,
+                        padding: EdgeInsets.zero,
+                        itemCount: 20,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 16.0),
+                            child: Container(
+                              padding: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.location_on,
+                                      color: Colors.grey[700]),
+                                  const SizedBox(width: 16.0),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Location $index',
+                                        style: const TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black87,
+                                        ),
                                       ),
-                                    ),
-                                    const SizedBox(height: 40.0),
-                                     Text(
-                                      'Description of location $index',
-                                      style: TextStyle(
-                                        fontSize: 14.0,
-                                        color: Colors.grey[800],
+                                      const SizedBox(height: 40.0),
+                                      Text(
+                                        'Description of location $index',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          color: Colors.grey[800],
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
-                   ),
                   ],
                 ),
               );
@@ -144,19 +185,19 @@ class Grabber extends StatelessWidget {
     return GestureDetector(
       onVerticalDragUpdate: onVerticalDragUpdate,
       child: Container(
-         width: double.infinity,
+        width: double.infinity,
         color: Colors.grey[300],
         child: Center(
-           child: Container(
+          child: Container(
             margin: const EdgeInsets.symmetric(vertical: 8.0),
             width: 32.0,
-           height: 4.0,
+            height: 4.0,
             decoration: BoxDecoration(
               color: Colors.grey[600],
               borderRadius: BorderRadius.circular(8.0),
             ),
           ),
-       ),
+        ),
       ),
     );
   }

3c29b06206f4fc5b5daa44d0f0566fe2820c4209
gokulsankar2 <gokulsankar2005@gmail.com>
2024-11-14 21:38:31 -0500

    Fixed Alert Widget

--- lib/alert.dart
+++ lib/alert.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'alert_base_class.dart';
 
 class CustomIconButton extends StatelessWidget {
   final IconData icon;
@@ -143,37 +145,34 @@ class AlertPage extends StatelessWidget {
   final Color welcomeBorder = const Color.fromRGBO(220, 220, 224, 1);
   final int datesShown = 14;
 
-
-
   @override
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> alertsArray = [
-        {
-          //mmddyy: month, date, year
-          "Date": 11072024,
-          "Title": "free houses",
-          "Description": "giving away houses to homeless people",
-          "isEmergency": true
-        },
-        {
-          "Date": 11072024,
-          "Title": "free apartments",
-          "Description": "giving away apartments to homeless people",
-          "isEmergency": false
-        }
-      ]
-    ;
+      {
+        //mmddyy: month, date, year
+        "Date": 11072024,
+        "Title": "free houses",
+        "Description": "giving away houses to homeless people",
+        "isEmergency": true
+      },
+      {
+        "Date": 11072024,
+        "Title": "free apartments",
+        "Description": "giving away apartments to homeless people",
+        "isEmergency": false
+      }
+    ];
     return Scaffold(
       backgroundColor: const Color(0xFFF4F4F4),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(110.0),
+        preferredSize: const Size.fromHeight(110.0),
         child: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xFF6359CA),
+          decoration: const BoxDecoration(
+            color: Color(0xFF6359CA),
           ),
           child: SafeArea(
             child: Padding(
-              padding: EdgeInsets.only(top: 50.0, bottom: 10.0),
+              padding: const EdgeInsets.only(top: 50.0, bottom: 10.0),
               child: Stack(
                 alignment: Alignment.center,
                 children: [
@@ -194,6 +193,9 @@ class AlertPage extends StatelessWidget {
                       fontWeight: FontWeight.w500,
                     ),
                   ),
+                  Alert(
+                      alertBase: AlertBase("Event", "Clean up today",
+                          "Gonna do some cleaning up today", DateTime.now())),
                 ],
               ),
             ),
@@ -233,8 +235,8 @@ class AlertPage extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10),
-                    Text(
+                    const SizedBox(height: 10),
+                    const Text(
                       'Missed a notification from us? No worries. They\'ll be right here waiting for you and for up to 14 days.',
                       style: TextStyle(
                         color: Colors.black,
@@ -246,30 +248,27 @@ class AlertPage extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: 
-              Listener( 
-                //this listener class makes it so that we should be able to add new alerts in the back end
-                child: ListView.builder(
-                          itemCount: datesShown,
+                child: Listener(
+              //this listener class makes it so that we should be able to add new alerts in the back end
+              child: ListView.builder(
+                itemCount: datesShown,
 
-                          //itembuilder that shows each date section
-                          itemBuilder: (BuildContext context, int dateindex) {
-                            return ListView.builder(
-                              itemCount: dateindex, //placeholder value
+                //itembuilder that shows each date section
+                itemBuilder: (BuildContext context, int dateindex) {
+                  return ListView.builder(
+                      itemCount: dateindex, //placeholder value
 
-                              //itemblder that shows each alert item in each date
-                              itemBuilder: (BuildContext context, int itemindex) { 
-                                return AlertItem(
-                                  isEmergency: alertsArray[itemindex]["isEmergency"],
-                                  alertTitle: alertsArray[itemindex]["Title"],
-                                  alertContent: alertsArray[itemindex]["Description"],
-                                );
-                              }
-                            );
-                          },
-                        ),
-              )
-            ),
+                      //itemblder that shows each alert item in each date
+                      itemBuilder: (BuildContext context, int itemindex) {
+                        return AlertItem(
+                          isEmergency: alertsArray[itemindex]["isEmergency"],
+                          alertTitle: alertsArray[itemindex]["Title"],
+                          alertContent: alertsArray[itemindex]["Description"],
+                        );
+                      });
+                },
+              ),
+            )),
           ],
         ),
       ),
@@ -287,7 +286,6 @@ class AlertItem extends StatelessWidget {
   });
 
   //add date sections
-
 
   final bool isEmergency;
   final String alertContent;
